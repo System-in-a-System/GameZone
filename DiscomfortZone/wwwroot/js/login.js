82807f397e1dca39cc73e81ed70af86b8f21ff28
Maryna <textconstruction@protonmail.com>
2021-06-07 16:18:29 +0200

    Introduce login status at startpage

--- DiscomfortZone/wwwroot/js/login.js
+++ DiscomfortZone/wwwroot/js/login.js
@@ -5,22 +5,6 @@ const usernameField = document.querySelector('#username')
 const passwordField = document.querySelector('#password')
 const commentField = document.querySelector('#comment')
 
-const onlineStatus = document.querySelector('#online-status')
-const loginSwitcher = document.querySelector('#login-switcher')
-
-if (loginSwitcher !== null) {
-    if (localStorage.getItem('loggedin') !== null) {
-        onlineStatus.innerText = localStorage.getItem('loggedin') + ' is playing 🎮'
-        loginSwitcher.innerText = 'Log off'
-    }
-
-
-    loginSwitcher.addEventListener('click', e => {
-        if (localStorage.getItem('loggedin') !== null) {
-            window.localStorage.removeItem('loggedin')
-        }
-    })
-}
 
 loginButton.addEventListener('click', e => {
     if (localStorage.getItem(usernameField.value) === passwordField.value) {
