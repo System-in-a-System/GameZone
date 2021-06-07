c12035de2125ae87cd5dceacae7b02d537cf7bd2
Maryna <textconstruction@protonmail.com>
2021-06-07 15:49:52 +0200

    Introduce login system

---
+++ DiscomfortZone/wwwroot/js/login.js
@@ -0,0 +1,33 @@
+﻿const loginButton = document.querySelector('#login-button')
+const form = document.querySelector('.form')
+
+const usernameField = document.querySelector('#username')
+const passwordField = document.querySelector('#password')
+const commentField = document.querySelector('#comment')
+
+const onlineStatus = document.querySelector('#online-status')
+const loginSwitcher = document.querySelector('#login-switcher')
+
+if (loginSwitcher !== null) {
+    if (localStorage.getItem('loggedin') !== null) {
+        onlineStatus.innerText = localStorage.getItem('loggedin') + ' is playing 🎮'
+        loginSwitcher.innerText = 'Log off'
+    }
+
+
+    loginSwitcher.addEventListener('click', e => {
+        if (localStorage.getItem('loggedin') !== null) {
+            window.localStorage.removeItem('loggedin')
+        }
+    })
+}
+
+loginButton.addEventListener('click', e => {
+    if (localStorage.getItem(usernameField.value) === passwordField.value) {
+        console.log(localStorage.getItem(usernameField.value))
+        commentField.innerText = 'Welcome, ' + usernameField.value
+        window.localStorage.setItem('loggedin', usernameField.value)
+    } else {
+        commentField.innerText = 'Username or password are invalid!'
+    }
+});
